3bf5609aff9b86170e96de7f25b1eaabd89598d2
leogdl <754324585@qq.com>
2023-05-09 13:30:53 +0800

    down stream task code update

---
+++ Stock-Prediction/data/sample.py
@@ -0,0 +1,17 @@
+[
+    [ # day 1
+        {'aspect1':0.1, ...., 'aspectd':0.1}, # comment 1
+        {'aspect1':0.2, ...., 'aspectd':0.2}, # comment 2
+        ...,
+        {'aspect1':0.1, ...., 'aspectd':0.1}, # comment m-1
+        {'aspect1':0.2, ...., 'aspectd':0.2}, # comment m
+    ],
+    ...
+    [ # day n
+        {'aspect1':0.1, ...., 'aspectd':0.1}, # comment 1
+        {'aspect1':0.2, ...., 'aspectd':0.2}, # comment 2
+        ...,
+        {'aspect1':0.1, ...., 'aspectd':0.1}, # comment m-1
+        {'aspect1':0.2, ...., 'aspectd':0.2}, # comment m
+    ]
+]
