d731df9d4b6eb12595d0e28fabe456e1c6448ad8
greenplumme <111282116+greenplumme@users.noreply.github.com>
2023-05-09 15:35:01 +0800

    Add files via upload

---
+++ Stock-Prediction/sentiment-analysis-using-bert.py
@@ -0,0 +1,431 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import re
+import csv
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from collections import defaultdict
+from textwrap import wrap
+
+import transformers
+from transformers import BertModel, BertTokenizer, RobertaConfig, RobertaModel, RobertaTokenizer, AdamW, get_linear_schedule_with_warmup
+import torch
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+
+# Misc.
+import warnings
+warnings.filterwarnings('ignore')
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+HAPPY_COLORS_PALETTE = ["#01BEFE", "#FFDD00", "#FF7D00", "#FF006D", "#ADFF02", "#8F00FF"]
+sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+rcParams['figure.figsize'] = 12, 8
+
+# Random seed for reproducibilty
+RANDOM_SEED = 42
+# Plot the distribution
+class_names = ['negative', 'neutral', 'positive']
+# Set the model name
+MODEL_NAME = 'bert-base-cased'
+# MODEL_NAME = 'roberta-base'
+MAX_LEN = 160
+
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
+
+# Set GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+df = pd.read_csv('./reviews.csv')
+# There are missing values in some of the columns but Content and score don't have a missing value. We can also look at the class balance. 
+# 
+# We will be alloting three classes:- 
+# 1. Positive (Score: 4-5)
+# 2. Neutral (Score: 3)
+# 3. Negative (Score: 1-2)
+
+
+# Function to convert score to sentiment
+def to_sentiment(rating):
+    
+    rating = int(rating)
+    # Convert to class
+    if rating <= 2: return 0
+    elif rating == 3: return 1
+    else: return 2
+
+def text_preprocessing(text):
+    """
+    - Remove entity mentions (eg. '@united')
+    - Correct errors (eg. '&amp;' to '&')
+    @param    text (str): a string to be processed.
+    @return   text (Str): the processed string.
+    """
+    # Remove '@name'
+    text = re.sub(r'(@.*?)[\s]', ' ', text)
+
+    # Replace '&amp;' with '&'
+    text = re.sub(r'&amp;', '&', text)
+
+    # Remove trailing whitespace
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
+
+# Apply to the dataset 
+df['sentiment'] = df.score.apply(to_sentiment)
+
+
+
+# Build a BERT based tokenizer
+# tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
+tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
+
+# Some of the common BERT tokens
+print(tokenizer.sep_token, tokenizer.sep_token_id) # marker for ending of a sentence
+print(tokenizer.cls_token, tokenizer.cls_token_id) # start of each sentence, so BERT knows we’re doing classification
+print(tokenizer.pad_token, tokenizer.pad_token_id) # special token for padding
+print(tokenizer.unk_token, tokenizer.unk_token_id) # tokens not found in training set 
+
+
+token_lens = []
+
+# Iterate through the content slide
+for txt in df.content:
+    tokens = tokenizer.encode(txt, max_length=MAX_LEN)
+    token_lens.append(len(tokens))
+
+class GPReviewDataset(Dataset):
+    # Constructor Function 
+    def __init__(self, reviews, targets, tokenizer, max_len):
+        self.reviews = reviews
+        self.targets = targets
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+    
+    # Length magic method
+    def __len__(self):
+        return len(self.reviews)
+    
+    # get item magic method
+    def __getitem__(self, item):
+        review = str(self.reviews[item])
+        target = self.targets[item]
+        
+        # Encoded format to be returned 
+        encoding = self.tokenizer.encode_plus(
+            text_preprocessing(review),
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation=True
+        )
+        
+        return {
+            'review_text': review,
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'targets': torch.tensor(target, dtype=torch.long)
+        }
+
+# Create a 80% train data and 10% test and 10% validation data
+df_train, df_test = train_test_split(df, test_size=0.2, random_state=RANDOM_SEED)
+df_val, df_test = train_test_split(df_test, test_size=0.5, random_state=RANDOM_SEED)
+
+print(df_train.shape, df_val.shape, df_test.shape)
+
+# Create a dataloader to release data in batches.
+def create_data_loader(df, tokenizer, max_len, batch_size):
+    ds = GPReviewDataset(
+        reviews=df.content.to_numpy(),
+        targets=df.sentiment.to_numpy(),
+        tokenizer=tokenizer,
+        max_len=max_len
+    )
+    
+    return DataLoader(
+        ds,
+        batch_size=batch_size,
+        num_workers=0
+    )
+
+
+# Create train, test and val data loaders
+BATCH_SIZE = 16
+train_data_loader = create_data_loader(df_train, tokenizer, MAX_LEN, BATCH_SIZE)
+val_data_loader = create_data_loader(df_val, tokenizer, MAX_LEN, BATCH_SIZE)
+test_data_loader = create_data_loader(df_test, tokenizer, MAX_LEN, BATCH_SIZE)
+
+# Examples 
+data = next(iter(train_data_loader))
+print(data.keys())
+
+print(data['input_ids'].shape)
+print(data['attention_mask'].shape)
+print(data['targets'].shape)
+
+# ## Sentiment Classification with BERT and Hugging Face
+# Build the Sentiment Classifier class 
+class SentimentClassifier(nn.Module):
+    
+    # Constructor class 
+    def __init__(self, n_classes):
+        super(SentimentClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained(MODEL_NAME)
+        self.drop = nn.Dropout(p=0.3)
+        self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+    
+    # Forward propagaion class
+    def forward(self, input_ids, attention_mask):
+        _, pooled_output = self.bert(
+          input_ids=input_ids,
+          attention_mask=attention_mask
+        )
+        #  Add a dropout layer 
+        output = self.drop(pooled_output)
+        return self.out(output)
+
+# We use a dropout layer for some regularization and a fully-connected layer for our output. We are returning the raw output of the last layer since that is required for the cross-entropy loss function in PyTorch to work. Create an instance and move it to the GPU
+
+# Instantiate the model and move to classifier
+model = SentimentClassifier(len(class_names))
+model = model.to(device)
+
+EPOCHS = 3
+
+# Optimizer Adam 
+optimizer = AdamW(model.parameters(), lr=3e-5, eps=1e-8 , correct_bias=False)
+
+total_steps = len(train_data_loader) * 3
+
+scheduler = get_linear_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps=500,
+    num_training_steps=total_steps
+)
+
+# Set the loss function 
+loss_fn = nn.CrossEntropyLoss().to(device)
+
+# Function for a single training iteration
+def train_epoch(model, data_loader, loss_fn, optimizer, device, scheduler, n_examples):
+    model = model.train()
+    losses = []
+    correct_predictions = 0
+    
+    for d in data_loader:
+        input_ids = d["input_ids"].to(device)
+        attention_mask = d["attention_mask"].to(device)
+        targets = d["targets"].to(device)
+        
+        outputs = model(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        
+        _, preds = torch.max(outputs, dim=1)
+        loss = loss_fn(outputs, targets)
+        correct_predictions += torch.sum(preds == targets)
+        losses.append(loss.item())
+        
+        # Backward prop
+        loss.backward()
+        
+        # Gradient Descent
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        scheduler.step()
+        optimizer.zero_grad()
+    
+    return correct_predictions.double() / n_examples, np.mean(losses)
+
+# evaluate
+def eval_model(model, data_loader, loss_fn, device, n_examples):
+    model = model.eval()
+    
+    losses = []
+    correct_predictions = 0
+    
+    with torch.no_grad():
+        for d in data_loader:
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            targets = d["targets"].to(device)
+            
+            # Get model ouptuts
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask
+            )
+            
+            _, preds = torch.max(outputs, dim=1)
+            loss = loss_fn(outputs, targets)
+            
+            correct_predictions += torch.sum(preds == targets)
+            losses.append(loss.item())
+            
+    return correct_predictions.double() / n_examples, np.mean(losses)
+
+# Write the training Loop and store the best training state.
+history = defaultdict(list)
+best_accuracy = 0
+
+for epoch in range(EPOCHS):
+    
+    # Show details 
+    print(f"Epoch {epoch + 1}/{EPOCHS}")
+    print("-" * 10)
+    
+    train_acc, train_loss = train_epoch(
+        model,
+        train_data_loader,
+        loss_fn,
+        optimizer,
+        device,
+        scheduler,
+        len(df_train)
+    )
+    
+    print(f"Train loss {train_loss} accuracy {train_acc}")
+    
+    # Get model performance (accuracy and loss)
+    val_acc, val_loss = eval_model(
+        model,
+        val_data_loader,
+        loss_fn,
+        device,
+        len(df_val)
+    )
+    
+    print(f"Val   loss {val_loss} accuracy {val_acc}")
+    print()
+    
+    history['train_acc'].append(train_acc)
+    history['train_loss'].append(train_loss)
+    history['val_acc'].append(val_acc)
+    history['val_loss'].append(val_loss)
+    
+    # If we beat prev performance
+    if val_acc > best_accuracy:
+        torch.save(model.state_dict(), 'best_model_state.bin')
+        best_accuracy = val_acc
+
+# The above took a lot of time but it's finally working. Now, we can plot the training and validation accuracy.
+
+# In[28]:
+model = SentimentClassifier(len(class_names)).to(device)
+model.load_state_dict(torch.load('best_model_state.bin'))
+
+# ### Model Evaluation
+test_acc, _ = eval_model(
+  model,
+  test_data_loader,
+  loss_fn,
+  device,
+  len(df_test)
+)
+
+test_acc.item()
+
+# Define a helper function to get predictions from our models. This is similar to the evaluation function, except that we’re storing the text of the reviews and the predicted probabilities
+def get_predictions(model, data_loader):
+    model = model.eval()
+
+    review_texts = []
+    predictions = []
+    prediction_probs = []
+    real_values = []
+
+    with torch.no_grad():
+        for d in data_loader:
+            texts = d["review_text"]
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            targets = d["targets"].to(device)
+
+            # Get outouts
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask
+            )
+            _, preds = torch.max(outputs, dim=1)
+
+            review_texts.extend(texts)
+            predictions.extend(preds)
+            prediction_probs.extend(outputs)
+            real_values.extend(targets)
+
+    predictions = torch.stack(predictions).cpu()
+    prediction_probs = torch.stack(prediction_probs).cpu()
+    real_values = torch.stack(real_values).cpu()
+
+    return review_texts, predictions, prediction_probs, real_values
+
+y_review_texts, y_pred, y_pred_probs, y_test = get_predictions(
+    model,
+    test_data_loader
+)
+
+
+print(classification_report(y_test, y_pred, target_names=class_names))
+
+def show_confusion_matrix(confusion_matrix):
+    hmap = sns.heatmap(confusion_matrix, annot=True, fmt="d", cmap="Blues")
+    hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation=0, ha='right')
+    hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation=30, ha='right')
+    plt.ylabel('True sentiment')
+    plt.xlabel('Predicted sentiment')
+
+cm = confusion_matrix(y_test, y_pred)
+df_cm = pd.DataFrame(cm, index=class_names, columns=class_names)
+show_confusion_matrix(df_cm)
+
+
+# review_text = "I love completing my todos! Best app ever!!!"
+df = pd.read_csv("lg_translated.csv", encoding = "ISO-8859-1")
+review_texts = df['text']
+times = df['time']
+
+with open('prediction_lg_bert.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Time', 'Sentiment']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writerow({'Time':'Time', 'Sentiment':'Sentiment'})
+
+for idx in range(0, len(review_texts)):
+    if review_texts[idx] == 'None':
+        prediction = 1
+    else:
+        encoded_review = tokenizer.encode_plus(
+            review_texts[idx],
+            max_length=MAX_LEN,
+            add_special_tokens=True,
+            return_token_type_ids=False,
+            pad_to_max_length=True,
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation=True
+        )
+
+
+        input_ids = encoded_review['input_ids'].to(device)
+        attention_mask = encoded_review['attention_mask'].to(device)
+
+        output = model(input_ids, attention_mask)
+        _, prediction = torch.max(output, dim=1)
+
+    with open('prediction_lg_bert.csv', 'a', newline='',encoding='utf-8') as csvfile:
+        fieldnames = ['Time', 'Sentiment']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=",")
+        writer.writerow({'Time':times[idx], 'Sentiment':class_names[prediction]})
+
